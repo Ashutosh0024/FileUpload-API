bce02f5364e13f522f2036c46191bbaf1ddaaf0e
Ashutosh <ashutosh2001sharma@gmail.com>
2022-11-08 18:57:28 +0530

    Springboot API

---
+++ src/main/java/com/filemngt/service/AttachmentService.java
@@ -0,0 +1,10 @@
+package com.filemngt.service;
+
+import com.filemngt.entity.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AttachmentService {
+    Attachment saveAttachment(MultipartFile file) throws Exception;
+
+    Attachment getAttachment(String fileId) throws Exception;
+}
